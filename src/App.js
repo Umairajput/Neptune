40e4a193af464dbf36a5ab242698490ea921dc74
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-01-12 18:04:36 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Navbar from './Components/Navbar/navbar';
-
+import { Route, Routes } from 'react-router';
 function App() {
   return (
     <>
-        <Navbar/>
+      <Navbar />
     </>
   );
 }
