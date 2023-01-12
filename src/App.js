e57bed0fe17f9c74ebe52a4e52163fe0eaa8c0ff
Zain_Ali_Sidd <114559080+Zain-Ali-Siddiqui@users.noreply.github.com>
2023-01-12 18:27:16 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/navbar';
 import Home from './Screens/Home/home';
-
+import About from './Screens/About/about';
+import Service from './Screens/Services/service';
+import { Route, Routes } from 'react-router-dom';
+import Contact from './Screens/Contact/contact';
 function App() {
   return (
     <>
-        <Navbar/>
-        <Home/>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Service" element={<Service />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Screens/Services/service.jsx
@@ -0,0 +1,8 @@
+function Service() {
+    return (
+        <>
+            <h1>This is Service Page...</h1>
+        </>
+    )
+}
+export default Service 
